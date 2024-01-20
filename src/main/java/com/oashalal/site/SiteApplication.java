438a0c1d84b8b75502ce7054ff217350c7bc93db
Your Name <you@example.com>
2024-01-20 13:09:03 +0300

    start

--- src/main/java/com/oashalal/site/SiteApplication.java
+++ src/main/java/com/oashalal/site/SiteApplication.java
@@ -2,6 +2,10 @@ package com.oashalal.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class SiteApplication {
@@ -9,5 +13,9 @@ public class SiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
 	}
-
+		
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    } 
 }
