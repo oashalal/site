40f8f58e1a936613c5ba1e87756d732860f4b886
Your Name <you@example.com>
2024-01-20 13:22:02 +0300

    controller fix

--- src/main/java/com/oashalal/site/SiteApplication.java
+++ src/main/java/com/oashalal/site/SiteApplication.java
@@ -6,8 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
 public class SiteApplication {
 
 	public static void main(String[] args) {
