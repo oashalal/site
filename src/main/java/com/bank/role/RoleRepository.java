addac37d56f3e839376fbf8e751ed35902d4631a
Your Name <you@example.com>
2024-01-26 18:54:26 +0300

    auto

--- src/main/java/com/bank/role/RoleRepository.java
+++ src/main/java/com/bank/role/RoleRepository.java
@@ -5,6 +5,8 @@ import com.bank.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     public Optional<Role> findByName(String name);
